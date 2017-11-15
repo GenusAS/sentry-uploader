f2ba07e9e2cfd5e20dfd4f2c9a3b91afe5badb2c
JEI <jei@genus.no>
2017-11-15 14:18:35 +0100

    Organization and project are now environment variables

--- functions/sentry.js
+++ functions/sentry.js
@@ -1,14 +1,14 @@
-const fs      = require('fs')
+const fs = require('fs')
 const request = require('request')
 
 const findFilesToUpload = require('./findFiles')
 
 
-const sentryToken = process.env.SENTRY_TOKEN 
+const sentryToken = process.env.SENTRY_TOKEN
 const auth = 'Bearer ' + sentryToken
 const baseUrl = 'https://sentry.io/api/0/projects/'
-const project = 'analysis-viewer'
-const organization = 'genus-as'
+const project = process.env.PROJECT_NAME
+const organization = process.env.ORGANIZATION
 const sentryUrl = baseUrl + organization + '/' + project + '/releases/'
 
 /**
@@ -16,88 +16,88 @@ const sentryUrl = baseUrl + organization + '/' + project + '/releases/'
  */
 exports.createRelease = (versionNumber) => new Promise((resolve, reject) => {
 
-    console.log("Creating release")
-    console.log("   with release number " + versionNumber)
+	console.log("Creating release")
+	console.log("   with release number " + versionNumber)
 
-    let options = {
-            url: sentryUrl,
-            method: 'POST',
-            headers: {
-                'Authorization': auth,
-                'Content-Type': 'application/json',
-            },
-            body: '{"version": "' + versionNumber + '"}',
-        }
-    
+	let options = {
+		url: sentryUrl,
+		method: 'POST',
+		headers: {
+			'Authorization': auth,
+			'Content-Type': 'application/json',
+		},
+		body: '{"version": "' + versionNumber + '"}',
+	}
 
-    request(
-        options,
-        function(error, response, body) {
-            if (error ) {
-                console.log('ERROR')
-                reject()
-            }
 
-            resolve(versionNumber)
-        }
-    )
+	request(
+		options,
+		function (error, response, body) {
+			if (error) {
+				console.log('ERROR')
+				reject()
+			}
+
+			resolve(versionNumber)
+		}
+	)
 
 })
-    
 
-exports.uploadSourceMaps = function(versionNumber) {
-    console.log("Uploading source maps")
-    
-    let files = findFilesToUpload()
-        .then(function(files) {
 
-            let url = sentryUrl + versionNumber+'/files/'
-        
-            files.forEach(file => {
-                let fileName = '~'+file.replace(/\\/g, '/').replace('bundle','')
-                request(
-                    {
-                        url: url,
-                        method: 'POST',
-                        headers: {
-                            'Authorization': auth
-                        },
-                        formData: {
-                            name: fileName,
-                            file: fs.createReadStream(file)
-                        }
-                    },  function(error, response, body) {
-                        console.log('error:', error) // Print the error if one occurred
-                        console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-                        console.log('body:', body) // Print the HTML for the Google homepage.
+exports.uploadSourceMaps = function (versionNumber) {
+	console.log("Uploading source maps")
 
-                    }
-                )
-            })
-        })
+	let files = findFilesToUpload()
+		.then(function (files) {
+
+			let url = sentryUrl + versionNumber + '/files/'
+
+			files.forEach(file => {
+				let fileName = '~' + file.replace(/\\/g, '/').replace('bundle', '')
+				request(
+					{
+						url: url,
+						method: 'POST',
+						headers: {
+							'Authorization': auth
+						},
+						formData: {
+							name: fileName,
+							file: fs.createReadStream(file)
+						}
+					}, function (error, response, body) {
+						console.log('error:', error) // Print the error if one occurred
+						console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+						console.log('body:', body) // Print the HTML for the Google homepage.
+
+					}
+				)
+			})
+		})
 }
 
 
-exports.deleteRelease = function(versionNumber) {
+exports.deleteRelease = function (versionNumber) {
 
-    let url = sentryUrl + versionNumber +'/'
-    console.log(url)
-    let options = {
-        url: url,
-        method: 'DELETE',
-        headers: {
-            'Authorization': auth
-        },
-    }
-    
+	let url = sentryUrl + versionNumber + '/'
+	console.log(url)
+	let options = {
+		url: url,
+		method: 'DELETE',
+		headers: {
+			'Authorization': auth
+		},
+	}
 
-    request(
-        options,
-        function(error, response, body) {
-            console.log('error:', error) // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-            console.log('body:', body) // Print the HTML for the Google homepage.
 
-        }
-    )
+	request(
+		options,
+		function (error, response, body) {
+			console.log('error:', error) // Print the error if one occurred
+			console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+			console.log('body:', body) // Print the HTML for the Google homepage.
+
+		}
+	)
 }
